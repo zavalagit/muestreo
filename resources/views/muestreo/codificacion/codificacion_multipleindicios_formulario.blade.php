f8765d382e732211f3c3b491530ceda1a96062d1
jess-github <jjcr.fie@gmail.com>
2022-03-29 11:11:27 -0600

    prueba rama

--- resources/views/muestreo/codificacion/codificacion_multipleindicios_formulario.blade.php
+++ resources/views/muestreo/codificacion/codificacion_multipleindicios_formulario.blade.php
@@ -11,7 +11,7 @@
 @section('titulo')
    FORMULARIO DE CODIFICACION
 @endsection
-@section('seccion', 'REGISTRAR CODIFICACION papirriquis')
+@section('seccion', 'REGISTRAR CODIFICACION prueba rama')
 
 
 @section('contenido')
