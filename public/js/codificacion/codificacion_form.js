729bc9d3660fa24d4b48a5e0d1afa61728068fc6
zavala <zavala@example.com>
2022-04-25 12:04:46 -0500

    modificacion  alertify registro reailzado

--- public/js/codificacion/codificacion_form.js
+++ public/js/codificacion/codificacion_form.js
@@ -11,47 +11,47 @@ $(function(){
          data: $(this).serialize(),
          success: function(respuesta){
             //console.log(respuesta);
-            console.log(respuesta.array_codificacion_id);
+            //console.log(respuesta.array_codificacion_id);
 
-            //if(respuesta.status){
-               // if (respuesta.formAccion == 'prestar') {
-               //    $('#btn-prestar').parent().addClass('ocultar');
-               //    // $('#btn-realizar-reingreso').attr('href',route('prestamo_form',{formAccion:'reingresar',cadena:$('#btn-realizar-reingreso').attr('data-cadena-id'),prestamo:respuesta.prestamo.id}));
-               //    // $('#btn-realizar-reingreso').attr('href',route('prestamo_form',{formAccion:'reingresar',cadena:$('#btn-realizar-reingreso').attr('data-cadena-id'),prestamo:respuesta.prestamo.id}));
-               //    // $('#btn-realizar-reingreso').parent().removeClass('ocultar');
-               //    $('#btn-prestamo-pdf').parent().removeClass('ocultar');
-               //    $('#btn-prestamo-pdf').attr('href', respuesta.prestamo_multiple ? '/bodega/prestamo-multiple-pdf/'+respuesta.array_prestamos_id : '/bodega/prestamo-pdf/'+respuesta.prestamo.id);
-               //    alertify.success("Prestamo realizado");
-               // }
-               // else if( respuesta.formAccion == 'reingresar' ){
-               //    alertify.success("Reingreso realizado");
+            if(respuesta.status){
+                  if (respuesta.formAccion == 'registrar') {
+                  //    $('#btn-prestar').parent().addClass('ocultar');
+                  //    // $('#btn-realizar-reingreso').attr('href',route('prestamo_form',{formAccion:'reingresar',cadena:$('#btn-realizar-reingreso').attr('data-cadena-id'),prestamo:respuesta.prestamo.id}));
+                  //    // $('#btn-realizar-reingreso').attr('href',route('prestamo_form',{formAccion:'reingresar',cadena:$('#btn-realizar-reingreso').attr('data-cadena-id'),prestamo:respuesta.prestamo.id}));
+                  //    // $('#btn-realizar-reingreso').parent().removeClass('ocultar');
+                  //    $('#btn-prestamo-pdf').parent().removeClass('ocultar');
+                  //    $('#btn-prestamo-pdf').attr('href', respuesta.prestamo_multiple ? '/bodega/prestamo-multiple-pdf/'+respuesta.array_prestamos_id : '/bodega/prestamo-pdf/'+respuesta.prestamo.id);
+                     alertify.success("Prestamo realizado");
+                  }
+                  // else if( respuesta.formAccion == 'reingresar' ){
+                  //    alertify.success("Reingreso realizado");
 
-               //    setTimeout(() => {
-               //       window.close();
-               //    }, 5000);
-               // }
-               // else if( respuesta.formAccion == 'editar' ){
-               //    alertify.success("Edición realizada");
+                  //    setTimeout(() => {
+                  //       window.close();
+                  //    }, 5000);
+                  // }
+                  // else if( respuesta.formAccion == 'editar' ){
+                  //    alertify.success("Edición realizada");
 
-               //    setTimeout(() => {
-               //       window.close();
-               //    }, 5000);
-               // }
+                  //    setTimeout(() => {
+                  //       window.close();
+                  //    }, 5000);
+                  // }
 
-               // // if (prestamo_tipo == 'prestamo-unico') {
-               // //    $('#btn-prestamo-pdf').attr('href','/bodega/prestamo-pdf/'+respuesta.prestamo_pdf);
-               // // }
-               // // else if(prestamo_tipo == 'prestamo-multiple'){
-               // //    $('#btn-prestamo-pdf').attr('href','/bodega/prestamo-multiple-pdf/'+respuesta.prestamo_pdf);
-               // // }
-               
+                  // // if (prestamo_tipo == 'prestamo-unico') {
+                  // //    $('#btn-prestamo-pdf').attr('href','/bodega/prestamo-pdf/'+respuesta.prestamo_pdf);
+                  // // }
+                  // // else if(prestamo_tipo == 'prestamo-multiple'){
+                  // //    $('#btn-prestamo-pdf').attr('href','/bodega/prestamo-multiple-pdf/'+respuesta.prestamo_pdf);
+                  // // }
+                  
 
-               // $('.indicio-checkbox').prop('disabled',true);
-            // }
-            // else{
-            //    console.log('algo anda mal');
-            //    console.log(prestamo.prestamo_pdf);
-            //}
+                  // $('.indicio-checkbox').prop('disabled',true);
+             }
+             else{
+                console.log('algo anda mal');
+                //console.log(prestamo.prestamo_pdf);
+             }
          },
          error: function(respuesta){
             console.log(respuesta);
