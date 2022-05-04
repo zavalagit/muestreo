14a00b2d3d38db3ad6356e403805d6fbe4b548f3
jess-github <jjcr.fie@gmail.com>
2022-05-04 09:38:30 -0500

    menu expedientes

--- resources/views/plantilla/menu_colectivos.blade.php
+++ resources/views/plantilla/menu_colectivos.blade.php
@@ -1,13 +1,13 @@
 <li class="item-menu submenu">
    <ul class="collapsible expandable">
       <li id="submenu-colectivos" class="submenu">
-         <div class="menu-header collapsible-header" style="margin-bottom:10px;"><i class="fas fa-circle"></i>COLECTIVOS</div>
+         <div class="menu-header collapsible-header" style="margin-bottom:10px;"><i class="fas fa-circle"></i>EXPEDIENTES</div>
 
          <div class="menu-body collapsible-body">
-            <a href="/colectivo-form/registrar" id="vista-colectivo-registrar"><i class="fas fa-pen"></i><span>REGISTRAR</span></a>
+            <a href="{{route('expedientes.create')}}" id="vista-colectivo-registrar"><i class="fas fa-pen"></i><span>REGISTRAR</span></a>
          </div>
          <div class="menu-body    collapsible-body">
-            <a href="/colectivo-consultar" id="vista-colectivo-consultar"><i class="fas fa-file-alt"></i><span>CONSULTAR</span></a>
+            <a href="{{route('expedientes.index')}}" id="vista-colectivo-consultar"><i class="fas fa-file-alt"></i><span>CONSULTAR</span></a>
          </div>
       </li>
    </ul>
